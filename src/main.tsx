7f0d30d16ac0afa8585d8f92acf92e90690918e5
Shubham Gupta <shubham01dec04@gmail.com>
2025-09-03 13:02:27 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,21 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+function getBasename() {
+  // Extract redirect query param if present
+  const params = new URLSearchParams(window.location.search);
+  const redirect = params.get("redirect");
+  if (redirect) {
+    // Replace URL with original route (clean)
+    const newUrl = window.location.origin + "/resume_spg" + redirect;
+    window.history.replaceState(null, "", newUrl);
+  }
+  return "/resume_spg/";
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/resume_spg/">
+    <BrowserRouter basename={getBasename()}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
